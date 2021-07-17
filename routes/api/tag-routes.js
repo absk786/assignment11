64681d947a082b0f0784a2ce2571483984815233
Abdul Khalil <rehmank11@gmail.com>
2021-07-17 20:31:56 +0100

    routs started for all of them

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,92 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
+  Tag.findAll().then(dbTagData => {
+    res.json(dbTagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findOne({
+    where:{
+      id:req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if(!dbTagData) {
+      res.status(400).json({message:"this tag does not exist"})
+      return
+    }
+    res.json(dbTagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
   // be sure to include its associated Product data
 });
 
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+Tag.create({
+  tag_name: "newTag"
+})
+.then(dbTagData => {
+  res.json(dbTagData)
+})
+.catch(err => {
+  console.log(err)
+  res.status(500).json(err)
+})
 });
 
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.Update({
+    where: {
+      id:req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({
+        message:"this tag was not found"
+      })
+      return;
+    }
+    res.json(dbTagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
+// delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+      id:req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({message:"this tag was not found"})
+      return
+    }
+    res.json(dbTagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
+
 
 module.exports = router;
